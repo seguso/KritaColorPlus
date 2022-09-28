abc562835f7733f17ebdaf051f95663d82bf783f
maurizio <maurizio.colucci@gmail.com>
2022-09-28 11:08:23 +0200

    added functions to mimic wet paper

--- pykrita/recent_color/recent_color.py
+++ pykrita/recent_color/recent_color.py
@@ -33,6 +33,16 @@ import datetime
 import xml
 
 
+class rgb:
+    def __init__(self, r, g, b, a):
+        self.a = a
+        self.r = r
+        self.g = g
+        self.b = b
+        
+    def print(self, msg):
+        print(f"{msg}:   r:{self.r}, g:{self.g}, b:{self.b} ,a:{self.a}")
+
 
 def get_q_view(view):
     window = view.window()
@@ -44,6 +54,25 @@ def get_q_view(view):
             return q_mdi_view.widget()
 
 
+def calcolaCompositeColor(colors):
+    mergedColor = None
+    for col in colors:
+            col.print("cur color")
+            if mergedColor is None:
+                mergedColor = col
+                mergedColor.print("setto merged color")
+            else:
+                a = float(col.a) / 255.0
+                print (f"a = {a}")
+                invA = (1.0 - a) 
+                print (f"invA = {invA}")
+                mergedColor = rgb( mergedColor.r * invA + col.r * a,
+                                        mergedColor.g * invA + col.g * a,
+                                        mergedColor.b* invA + col.b * a,
+                                        255)
+                mergedColor.print("merged color")
+    return mergedColor
+    
 def get_q_canvas(q_view):
     scroll_area = q_view.findChild(QAbstractScrollArea)
     viewport = scroll_area.viewport()
@@ -209,8 +238,7 @@ class MyExtension(Extension):
                 #self.timer_hm.stop()
                 raise
 
-
-    def mix(self):
+    def mixOld(self):
         app = Krita.instance()
         win = app.activeWindow()
         if win is not None:
@@ -245,7 +273,92 @@ class MyExtension(Extension):
                     view.setForeGroundColor(fg)
 
         
-    def pick(self):
+
+    def mix(self):
+        app = Krita.instance()
+        win = app.activeWindow()
+        if win is not None:
+            view = win.activeView()
+            if view is not None:
+                document = view.document()
+                if document:
+                    center = QPointF(0.5 * document.width(), 0.5 * document.height())
+                    p = get_cursor_in_document_coords()
+                    doc_pos = p + center
+                    print(f'cursor at: x={doc_pos.x()}, y={doc_pos.y()}')
+                    
+                    
+                    parentNode = document.activeNode().parentNode()
+                    
+                    
+                    if parentNode is not None:
+                    
+                        brothers = parentNode.childNodes()
+                        colors = []
+                        
+                        #costruisco colors
+                        for curLayer in brothers:
+                        
+                            self.pixelBytes = curLayer.pixelData(doc_pos.x(), doc_pos.y(), 1, 1)
+                            
+                            self.imageData = QImage(self.pixelBytes, 1, 1, QImage.Format_RGBA8888)
+                            self.pixelC = self.imageData.pixelColor(0,0)
+                            print(f"color under cursor =  r:{self.pixelC.red()}, g:{self.pixelC.green()}, b:{self.pixelC.blue()} ,a:{self.pixelC.alpha()}")
+                            
+                            colors.append(  rgb(self.pixelC.red(),  self.pixelC.green(),  self.pixelC.blue(),  self.pixelC.alpha() ))
+                        
+                        #creo il colore composito
+                        
+                        mergedColor = calcolaCompositeColor(colors);
+                                                        
+                                
+
+                                
+                        # setto il fg color uguale a merged color mischiato con il fg
+                        fg = view.foregroundColor()
+                        comp = fg.components() 
+                        print(f"fg color = {comp}")
+         
+         
+                        
+                        canv = 0.45
+                        fgMul = 1.0 - canv
+                        comp[0] = comp[0] * fgMul + (mergedColor.r / 255.0)  * canv
+                        comp[1] = comp[1] * fgMul + (mergedColor.g / 255.0)  * canv
+                        comp[2] = comp[2] * fgMul + (mergedColor.b  / 255.0)  * canv
+                        
+                        # comp[0] =  (mergedColor.r / 255.0)  
+                        # comp[1] =  (mergedColor.g / 255.0)
+                        # comp[2] = (mergedColor.b  / 255.0)
+                      
+                        fg.setComponents(comp)
+                        
+                        view.setForeGroundColor(fg)
+                        
+                        
+                    # self.pixelBytes = document.activeNode().pixelData(doc_pos.x(), doc_pos.y(), 1, 1)
+                    
+                    # self.imageData = QImage(self.pixelBytes, 1, 1, QImage.Format_RGBA8888)
+                    # self.pixelC = self.imageData.pixelColor(0,0)
+                    # print(f"color under cursor = {self.pixelC.red()}, {self.pixelC.green()}, {self.pixelC.blue()}")
+                    
+                    # fg = view.foregroundColor()
+                    # comp = fg.components() 
+                    # print(f"fg color = {comp}")
+     
+     
+                    # canv = 0.45
+                    # fgMul = 1.0 - canv
+                    # comp[0] = comp[0] * fgMul + (self.pixelC.red() / 255.0)  * canv
+                    # comp[1] = comp[1] * fgMul + (self.pixelC.green() / 255.0)  * canv
+                    # comp[2] = comp[2] * fgMul + (self.pixelC.blue()  / 255.0)  * canv
+                  
+                    # fg.setComponents(comp)
+                    
+                    # view.setForeGroundColor(fg)
+
+        
+    def pickOld(self):
         app = Krita.instance()
         win = app.activeWindow()
         if win is not None:
@@ -276,6 +389,81 @@ class MyExtension(Extension):
                     
                     view.setForeGroundColor(fg)
 
+
+
+                                
+    def pick(self):
+        app = Krita.instance()
+        win = app.activeWindow()
+        if win is not None:
+            view = win.activeView()
+            if view is not None:
+                document = view.document()
+                if document:
+                    center = QPointF(0.5 * document.width(), 0.5 * document.height())
+                    p = get_cursor_in_document_coords()
+                    doc_pos = p + center
+                    print(f'cursor at: x={doc_pos.x()}, y={doc_pos.y()}')
+                    
+                    parentNode = document.activeNode().parentNode()
+                    
+                    
+                    if parentNode is not None:
+                    
+                        brothers = parentNode.childNodes()
+                        colors = []
+                        
+                        #costruisco colors
+                        for curLayer in brothers:
+                        
+                            self.pixelBytes = curLayer.pixelData(doc_pos.x(), doc_pos.y(), 1, 1)
+                            
+                            self.imageData = QImage(self.pixelBytes, 1, 1, QImage.Format_RGBA8888)
+                            self.pixelC = self.imageData.pixelColor(0,0)
+                            print(f"color under cursor =  r:{self.pixelC.red()}, g:{self.pixelC.green()}, b:{self.pixelC.blue()} ,a:{self.pixelC.alpha()}")
+                            
+                            colors.append(  rgb(self.pixelC.red(),  self.pixelC.green(),  self.pixelC.blue(),  self.pixelC.alpha() ))
+                        
+                        #creo il colore composito
+                        
+                        mergedColor = calcolaCompositeColor(colors);
+                                                        
+                                
+
+                                
+                        # setto il fg color uguale a merged color
+                        fg = view.foregroundColor()
+                        comp = fg.components() 
+                        print(f"fg color = {comp}")
+         
+                        comp[0] =  (mergedColor.r / 255.0)  
+                        comp[1] =  (mergedColor.g / 255.0)
+                        comp[2] = (mergedColor.b  / 255.0)
+                      
+                        fg.setComponents(comp)
+                        
+                        view.setForeGroundColor(fg)
+
+    
+    
+    def layerMergeAndCreate(self):
+        application = Krita.instance()
+        currentDoc = application.activeDocument()
+        activeLayer = currentDoc.activeNode()
+        parentNode = activeLayer.parentNode()
+        if parentNode is not None:
+            oldOpacity = activeLayer.opacity()
+            activeLayer.mergeDown()
+            root = currentDoc.rootNode()
+            newLa = currentDoc.createNode("temp_layer", "paintLayer")
+            newLa.setOpacity(oldOpacity)
+            parentNode.addChildNode(newLa, None)
+        else:
+            newLa = currentDoc.createNode("temp_layer", "paintLayer")
+            newLa.setOpacity(160)
+            root.addChildNode(newLa, None)
+            
+        
     
     def createActions(self, window):
     
@@ -290,7 +478,10 @@ class MyExtension(Extension):
         action2 = window.createAction("PickColor", "PickColor")
         action2.triggered.connect(self.pick)
 
+        action2 = window.createAction("LayerMergeDownAndNew", "LayerMergeDownAndNew")
+        action2.triggered.connect(self.layerMergeAndCreate)
 
+        
 # And add the extension to Krita's list of extensions:
 Krita.instance().addExtension(MyExtension(Krita.instance()))
 
