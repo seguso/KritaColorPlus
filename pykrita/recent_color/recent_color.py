183f73a3ba3954c6bc72e5659bec92785ecf146b
Maurizio Colucci <maurizio.colucci@gmail.com>
2025-04-09 18:51:44 +0200

    fix crash nuovo in automix

--- pykrita/recent_color/recent_color.py
+++ pykrita/recent_color/recent_color.py
@@ -3026,7 +3026,7 @@ class MyExtension(Extension):
                                                 pixelC = imageData.pixelColor(0,0)
                                                 
                                                 #e ora da colore qt a colore mio 
-                                                mergedColor = rgb(pixelC.red(),  pixelC.green(),  pixelC.blue(), 255.0)
+                                                mergedColor = rgb(float(pixelC.red()),  float(pixelC.green()),  float(pixelC.blue()), 255.0)
                                                 
                                                 
                                                 # for pos in positions:
